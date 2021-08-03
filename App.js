cc58b2af6b7035a0dceb85911a41f30ef9035f50
Sufian <sufian@muslimkids.tv>
2021-08-03 09:34:53 +0500

    Integrated JWPlayer for Android.

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, View} from 'react-native';
+import {Dimensions, SafeAreaView, StyleSheet, View} from 'react-native';
+import JWPlayer from 'react-native-jw-media-player';
+
+const {width} = Dimensions.get('window');
 
 const App: () => Node = () => {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.subContainer}>
-        <View style={styles.playerContainer}></View>
+        <View style={styles.playerContainer}>
+          <JWPlayer
+            style={styles.player}
+            playlistItem={{
+              mediaId: '1',
+              file: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/CastVideos/mp4/BigBuckBunny.mp4',
+              autostart: true,
+            }}
+            onBeforePlay={() => console.log('onBeforePlay()')}
+            onPlay={() => console.log('onPlay()')}
+            onPause={() => console.log('onPause()')}
+            onIdle={() => console.log('onIdle')}
+            onSetupPlayerError={event => console.log('onPlayerError(event)')}
+            onPlayerError={event => console.log('onPlayerError(event)')}
+            onFullScreen={() => console.log('onFullScreen()')}
+            onFullScreenExit={() => console.log('onFullScreenExit()')}
+            nativeFullScreen={true} // when undefined or false you will need to handle the player styles in onFullScreen & onFullScreenExit callbacks
+          />
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -24,8 +45,11 @@ const styles = StyleSheet.create({
   },
   playerContainer: {
     height: 300,
-    width: 500,
+    width: width - 40,
     margin: 40,
+  },
+  player: {
+    flex: 1,
   },
 });
 
